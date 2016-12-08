74da0a3a25b1011cb232328bbf3b71d971872fd5
Anton <toh1napocht@yandex.ru>
2016-12-09 00:21:44 +0200

    use broscript

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 var restify = require('restify');
-//var express = require('express');
 var builder = require('botbuilder');
+var broscript = require('broscript');
+
+//BROSCRIPT_USER_KEY and BROSCRIPT_SCRIPT_KEY heroku variables https://devcenter.heroku.com/articles/config-vars
+broscript.config.userKey = process.env.BROSCRIPT_USER_KEY;
+broscript.config.scriptKey = process.env.BROSCRIPT_SCRIPT_KEY;
+
+//var controller = broscript.APIController;
 
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 5000, function () {
@@ -8,27 +14,29 @@ server.listen(process.env.port || process.env.PORT || 5000, function () {
 });
 
 // Create bot and add dialogs
+//MS_APP_ID and MS_APP_PASSWORD heroku variables https://devcenter.heroku.com/articles/config-vars
 var connector = new builder.ChatConnector({
-    appId: "3b34df50-d516-4b7c-ab00-cf08afe88d0a",
-    appPassword: "bi9HvaVx0TvK5uYFpwoHpWN"
+    appId: process.env.MS_APP_ID,
+    appPassword: process.env.MS_APP_PASSWORD
 });
 var bot = new builder.UniversalBot(connector);
-// server.post('/api/messages', connector.listen());
 server.post('/', connector.listen());
 
 
-bot.dialog('/', function (session) {
-    session.send('Hello World');
+bot.dialog('/', function (session, args, next) {
+    var options = {
+        "msg": session.message.text, //msg
+        "chatId":session.message.user.id, //chatId
+        "contact":null, //contact
+        "external":null, //external
+        "stopIsNull":null, //stopIsNull
+        "repeatIsNull":null //repeatIsNull
+    };
+    broscript.api.answers(options, function(error, response, context){ //callback
+        if(error !== null || response == null){
+            //send error
+        }else{
+            session.send(response.answer);
+        }
+    });
 });
-
-/*
-var app = express();
-
-app.get('/', function (req, res) {
-    res.send('Hello World!');
-});
-
-var port = process.env.PORT || 5000;
-app.listen(port, function () {
-    console.log('Example app listening on port 3000!');
-});*/
